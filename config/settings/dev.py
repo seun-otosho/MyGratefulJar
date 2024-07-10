a41a0fad6974a1e7cbaf02143444757c6674f41e
omniotosho <toloruntotosho@gmail.com>
2024-07-10 03:48:54 +0100

    commit the first

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-z82b=#(ad!g7pg_r%6b7nynany=6kdo4xfa2*e419k2%3*1+x)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
