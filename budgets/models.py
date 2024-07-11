2e53cb67573cc67fa1934022be1c648b78acaf15
omniotosho <toloruntotosho@gmail.com>
2024-07-11 01:33:33 +0100

    expense tags and str formatting

--- budgets/models.py
+++ budgets/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from taggit.models import TaggedItemBase
+from django import forms
 from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
 
@@ -31,10 +36,16 @@ class BudgetItem(index.Indexed, models.Model):
     class Meta:
         db_table = 'budgets'
         verbose_name = 'Budget Item'
-        unique_together = (('user', 'category', 'date', ),)
+        unique_together = (('user', 'category', 'date',),)
 
     def __str__(self):
-        return f'{self.user} - {self.category} - {self.date} - {self.amount}'
+        return f'Budget for {self.category} - {self.date:%b, %Y} - {self.amount:,}'
+
+    panels = [
+        FieldPanel("date"),
+        FieldPanel("amount"),
+        FieldPanel("category", widget=forms.RadioSelect),
+    ]
 
     search_fields = [
         index.SearchField('amount', partial_match=True),
@@ -53,13 +64,13 @@ class Income(index.Indexed, models.Model):
         db_table = 'income'
 
     def __str__(self):
-        return f"{self.date} - {self.amount} - {self.description} - {self.category}"
+        return f"{self.date:%b, %Y} - {self.amount:,} - {self.description} - {self.category}"
 
     panels = [
         FieldPanel("date"),
         FieldPanel("amount"),
         FieldPanel("description"),
-        FieldPanel("category"),
+        FieldPanel("category", widget=forms.RadioSelect),
     ]
 
     search_fields = [
@@ -68,19 +79,26 @@ class Income(index.Indexed, models.Model):
     ]
 
 
-class Expense(index.Indexed, models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_index=True,)
+class ExpenseTag(TaggedItemBase):
+    content_object = ParentalKey('Expense', related_name='tagged_expenses', on_delete=models.DO_NOTHING, )
+
+
+class Expense(ClusterableModel, index.Indexed, models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, db_index=True, )
     date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=55, db_index=True, )
     description = models.CharField(max_length=256, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
 
+    tags = ClusterTaggableManager(through=ExpenseTag, blank=True)
+
     panels = [
         FieldPanel("date"),
         FieldPanel("amount"),
         FieldPanel("description"),
-        FieldPanel("category"),
+        FieldPanel("category", widget=forms.RadioSelect),
+        FieldPanel("tags")
     ]
 
     search_fields = [
@@ -90,3 +108,6 @@ class Expense(index.Indexed, models.Model):
 
     class Meta:
         db_table = 'expenses'
+
+    def __str__(self):
+        return f'{self.category} of {self.amount:,} on {self.date:%b %d, %Y}'
