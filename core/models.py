fc2719a1b051a02799cce88dcf958a0537de906f
omniotosho <toloruntotosho@gmail.com>
2024-07-22 06:40:33 +0100

    allauth, branding and social media to core, nav cleanup

--- core/models.py
+++ core/models.py
@@ -4,6 +4,13 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django_countries.fields import CountryField
 from django.utils.translation import gettext_lazy as _
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+from wagtail.contrib.forms.models import AbstractFormField, AbstractEmailForm
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+from wagtail.contrib.settings.models import BaseGenericSetting
+from wagtail.contrib.settings.registry import register_setting
+from wagtail.fields import RichTextField
 
 USERNAME_REGEX = '^[a-zA-Z0-9.@_]*$'
 
@@ -65,7 +72,7 @@ class User(AbstractUser, AbstractBaseUser, PermissionsMixin):
     country = CountryField()
 
     def __str__(self):
-        return self.email
+        return self.get_full_name() if self.get_full_name() != "" else self.username
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
@@ -84,3 +91,62 @@ class User(AbstractUser, AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         db_table = 'users'
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey('ContactPage', on_delete=models.DO_NOTHING, related_name='form_fields')
+
+
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
+
+    template = "core/nemesis/email_form_page.html"
+
+    def get_landing_page_template(self, *args, **kwargs):
+        return "core/nemesis/email_form_page_landing.html"
+
+
+@register_setting
+class SocialMediaLinks(BaseGenericSetting):
+    facebook = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedIn = models.URLField(blank=True, null=True)
+    x = models.URLField(blank=True, null=True)
+    youtube = models.URLField(blank=True, null=True)
+
+    panels = [
+        FieldPanel('github'),
+        FieldPanel('linkedIn'),
+        FieldPanel('x'),
+        FieldPanel('youtube'),
+    ]
+
+    class Meta:
+        db_table = "social_media"
+        verbose_name = "Social Media"
+
+
+@register_setting
+class BrandSettings(BaseGenericSetting):
+    # name = models.CharField(max_length=256)
+    logo = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.SET_NULL, null=True, blank=True, related_name="+",)
+
+    class Meta:
+        db_table = "brand"
