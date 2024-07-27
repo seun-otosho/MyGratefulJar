e93ccded4ab15f9cb292483ce6125e3e3f9d38c1
omniotosho <toloruntotosho@gmail.com>
2024-07-27 07:21:18 +0100

    prod & secrets

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,12 @@
 from .base import *
 
 DEBUG = False
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["mygratefuljar.com"]
 try:
-    from .local import *
+    from .mail import *
 except ImportError:
     pass
